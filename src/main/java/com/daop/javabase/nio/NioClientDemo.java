76aa674a7ef799ba68b6eb2128425ff200761762
Gadmind <daopudu@sina.com>
2020-11-02 16:26:52 +0800

    NIO----客户端优化

--- src/main/java/com/daop/javabase/nio/NioClientDemo.java
+++ src/main/java/com/daop/javabase/nio/NioClientDemo.java
@@ -22,6 +22,10 @@ import java.util.concurrent.TimeUnit;
  * @AUTHOR: Administrator
  **/
 public class NioClientDemo {
+
+    public static final int TIMEOUT = 1000;
+    public static final int BUFFER_SIZE = 1024;
+
     static ExecutorService threadPool = new ThreadPoolExecutor(10,
             20,
             3L, TimeUnit.SECONDS,
@@ -30,82 +34,141 @@ public class NioClientDemo {
 
     public static void main(String[] args) {
         NioClient nioClient = new NioClient("127.0.0.1", 8888);
-        threadPool.execute(() -> {
-            nioClient.read();
-            try{
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        threadPool.execute(nioClient);
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String msg = sc.nextLine();
-            nioClient.write(msg);
+            nioClient.sendMsg(msg);
+            nioClient.readMsg();
         }
     }
 
-    public static class NioClient {
+    public static class NioClient implements Runnable {
         private String host;
         private int port;
-        private SocketChannel socketChannel;
+
         private Selector selector;
-        private String username;
+        private SocketChannel socketChannel;
 
+        private volatile boolean stop;
 
-        NioClient(String host, int port) {
+        public NioClient(String host, int port) {
             this.host = host;
             this.port = port;
             try {
-                //打开一个通道，通道的另一头是这个服务端
-                //服务端的Selector中配置了服务端的ServerSocketChannel，与该ServerSocketChannel有关联的所有SocketChannel通道发送数据，都会默认把自己（当前通道）给放入Selector中。
-                socketChannel = SocketChannel.open(new InetSocketAddress(host, port));
-                //设置通道为非阻塞通道
+                //打开SocketChannel，创建客户端链接
+                socketChannel = SocketChannel.open();
+                //设置SocketChannel为非阻塞模式
                 socketChannel.configureBlocking(false);
-                //打开一个选择器
+                //创建多路复用器
                 selector = Selector.open();
-                //这个selector选择器监听该通道的读方法，这个通道为接收服务端消息的通道
-                socketChannel.register(selector, SelectionKey.OP_READ);
-                //得到username，ip地址:端口号
-                username = socketChannel.getLocalAddress().toString().substring(1);
-                System.out.println(username + " is ok...");
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-        public void read() {
+        @Override
+        public void run() {
             try {
-                //等待监听的事件发生，register(selector, SelectionKey.OP_READ);
-                selector.select();
-                //得到监听到的事件通道
-                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-                while (iterator.hasNext()) {
-                    SelectionKey key = iterator.next();
-                    iterator.remove();
-                    //这个通道为服务端发送数据的通道
-                    SocketChannel channel = (SocketChannel) key.channel();
-                    System.out.println("本地地址：" + channel.getLocalAddress());
-                    ByteBuffer buffer = ByteBuffer.allocate(1024 * 16);
-                    int read = channel.read(buffer);
-                    if (read > 0) {
-                        System.out.println(new String(buffer.array()));
+                //socketChannel发起连接
+                if (socketChannel.connect(new InetSocketAddress(host, port))) {
+                    //连接成功，注册到多路复用器上
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                } else {
+                    socketChannel.register(selector, SelectionKey.OP_CONNECT);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (true) {
+                readMsg();
+            }
+            // 多路复用器关闭后，所有注册在上面的Channel和Pipe等资源都会被自动去注册并关闭，所以不需要重复释放资源
+           /* if (selector != null) {
+                try {
+                    selector.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }*/
+        }
+
+        public void sendMsg(String msg) {
+            try {
+                //发送消息，读应答
+                byte[] req = msg.getBytes();
+                ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
+                writeBuffer.put(req);
+                writeBuffer.flip();
+                socketChannel.write(writeBuffer);
+                if (!writeBuffer.hasRemaining()) {
+                    System.out.println("Send order 2 server succeed.");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void readMsg() {
+            try {
+                //多路复用器轮询准备就绪的Key
+                selector.select(TIMEOUT);
+                Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+                SelectionKey key = null;
+                while (iter.hasNext()) {
+                    key = iter.next();
+                    iter.remove();
+                    try {
+                        if (key.isValid()) {
+                            //将链接成功的Channel注册到多路复用器上
+                            //判断连接是否成功
+                            SocketChannel sc = (SocketChannel) key.channel();
+                            if (key.isConnectable()) {
+                                if (sc.finishConnect()) {
+                                    sc.register(selector, SelectionKey.OP_READ);
+                                }
+                            }
+                            //监听读操作，读取服务器的网络信息
+                            try {
+                                if (key.isReadable()) {
+                                    ByteBuffer readBuffer = ByteBuffer.allocate(BUFFER_SIZE);
+                                    int readBytes = sc.read(readBuffer);
+                                    if (readBytes > 0) {
+                                        readBuffer.flip();
+                                        byte[] bytes = new byte[readBuffer.remaining()];
+                                        readBuffer.get(bytes);
+                                        String body = new String(bytes, "UTF-8");
+                                        System.out.println("Now is : " + body);
+                                    } else if (readBytes < 0) {
+                                        System.out.println("close ");
+                                        // 对端链路关闭
+                                        key.cancel();
+                                        sc.close();
+                                    }
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                System.out.println("Server close connection......");
+                                key.cancel();
+                                sc.close();
+                            }
+
+                        }
+                    } catch (IOException e) {
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
                     }
+
+
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-        }
-
-        public void write(String msg) {
-            msg = username + " 说：" + msg;
-            try {
-                //这个socketChannel就是当前通道，write直接发送给服务端的ServerSocketChannel，ServerSocketChannel收到通道后，会默认直接连接在服务端的selector中。
-                socketChannel.write(ByteBuffer.wrap(msg.getBytes()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 
     }
